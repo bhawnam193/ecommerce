67b0cc800a916ee68357bc4929bfb7976afc699d
Manik Malhotra <manik.malhotra27@gmail.com>
2020-02-22 14:21:15 +0530

    changes

--- ecommerce-front/src/auth/AdminRoute.jsx
+++ ecommerce-front/src/auth/AdminRoute.jsx
@@ -4,12 +4,13 @@ import { isAuthenticated } from './index';
 
 const AdminRoute = ({ component: Component, ...rest }) => {
 
-	const { user } = isAuthenticated();
+const { user } = isAuthenticated();
 
-	return (
-    	<Route {...rest} render={ props => user && user.role === 1 ? ( <Component {...props} /> ): ( <Redirect to={{pathname: '/signin', state: {from: props.location}}} /> ) }
-		/>
-	)
+return (
+    <Route {...rest} render={ props => user && user.role === 1 ? ( <Component {...props} />): (<Redirect to={{pathname: '/signin', state: {from: props.location}}} />)
+}
+/>
+)
 };
 
 export default AdminRoute;
