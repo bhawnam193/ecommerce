6527948ae010f8dbda8f24bd05de7e461f4213cd
Manik Malhotra <manik.malhotra27@gmail.com>
2020-03-03 08:37:31 +0530

    changes

--- ecommerce/controllers/order.js
+++ ecommerce/controllers/order.js
@@ -1,3 +1,28 @@
+const con = require('../db');
+
 exports.create = (req, res) => {
-    console.log('create order ', req.body);
+    //console.log(req.body.order.products);
+    try {
+        var sql = `INSERT INTO
+					  orders (products, transaction_id, amount, address, user)
+					VALUES
+					  ('${Object.assign({}, req.body.order.products)}', '${req.body.order.transaction_id}', '${req.body.order.amount}', '${req.body.order.address}', ${req.profile.id})`;
+
+        con.query(sql, function(err, result) {
+
+            if (err) throw err;
+
+            if (result.length) {
+                return res.status(200).json({ order_create: true });
+            } else {
+                return res.status(400).json({
+                    errors: [{ msg: 'An error occured while creating order' }]
+                });
+            }
+        });
+    } catch (err) {
+        return res.status(422).json({
+            errors: err.array()
+        });
+    }
 }
