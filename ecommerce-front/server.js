10be3781e8c5577cc36b664d52b1a207f6f16bcb
Manik Malhotra <manik.malhotra27@gmail.com>
2020-03-14 11:19:24 +0530

    changes

--- ecommerce-front/server.js
+++ ecommerce-front/server.js
@@ -4,6 +4,11 @@ const path = require('path');
 const app = express();
  
 app.use(compression());
+
+//force domain to be the url set in the env file
+console.log(process.env.FRONT_END);
+app.use( require('express-force-domain')(process.env.FRONT_END) );
+
 app.use(express.static(path.join(__dirname, 'build')));
  
 app.get('*', function(req, res) {
